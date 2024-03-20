abadee7e4ec55e49fca8b18b2d9d4f250baf257b
Janice <janice0975751227@gmail.com>
2024-03-21 00:56:22 +0800

    bro project

---
+++ projects/bio/quiz.js
@@ -0,0 +1,35 @@
+function AnsA(selectedanswer,btnElement){
+    if (selectedanswer == 'A'){
+        btnElement.style.backgroundColor = "green";
+    } else{
+        btnElement.style.backgroundColor = "red";
+        alert("Wrong");
+    }
+}
+
+function AnsB(selectedanswer,btnElement){
+    if (selectedanswer == 'B'){
+        btnElement.style.backgroundColor = "green";
+    } else{
+        btnElement.style.backgroundColor = "red";
+        alert("Wrong");
+    }
+}
+
+function AnsC(selectedanswer,btnElement){
+    if (selectedanswer == 'C'){
+        btnElement.style.backgroundColor = "green";
+    } else{
+        btnElement.style.backgroundColor = "red";
+        alert("Wrong");
+    }
+}
+
+function AnsD(selectedanswer,btnElement){
+    if (selectedanswer == 'D'){
+        btnElement.style.backgroundColor = "green";
+    } else{
+        btnElement.style.backgroundColor = "red";
+        alert("Wrong");
+    }
+}
